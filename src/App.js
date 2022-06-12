96f5ae4c2e14066b6ff27e9ccf9244307e63a26d
ysychshe <yevgeniy.sychshenko@external.t-systems.com>
2022-06-12 20:32:50 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+    const [toggle, setToggle] = useState(false);
+    const [value, setValue] = useState('');
+
+    const onClick = () => setToggle(prev => !prev)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setData({})
+        }, 100)
+    }, [])
+
+    return (
+        <div>
+            <h1 data-testid="value-elem">{value}</h1>
+            {toggle === true && <div data-testid="toggle-elem">toggle</div>}
+            {data && <div style={{color: 'red'}}>data</div>}
+            <h1>Hello world</h1>
+            <button data-testid="toggle-btn" onClick={onClick}>Click me</button>
+            <input onChange={e => setValue(e.target.value)} type="text" placeholder="input value..."/>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,61 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Test APP', () => {
+  test('check getByText, getByRole, getByPlaceholderText', () => {
+    render(<App />);
+    // getBy - выбросит ошибку если такого элемента нет
+    // queryBy - не выбросит ошибку если елемента нет
+    // findBy - используется для ассинхронных операций
+
+    const helloWorld = screen.getByText(/hello world/i);
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input value/i);
+
+    expect(helloWorld).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    // expect(input).toMatchSnapshot();
+    // screen.debug(); // log
+  });
+
+  test('check queryBy', () => {
+    render(<App />);
+    const textDoesntExist = screen.queryByText(/hello2/i)
+    expect(textDoesntExist).toBeNull();
+  });
+
+  test('check finBy', async() => {
+    render(<App />);
+    const data = await screen.findByText(/data/i);
+    expect(data).toBeInTheDocument();
+    expect(data).toHaveStyle({color: 'red'});
+  });
+
+  test('CLICK EVENT', async() => {
+    render(<App />);
+    const btn = screen.getByTestId('toggle-btn');
+    expect(screen.queryByTestId('toggle-elem')).toBeNull;
+
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument();
+
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-elem')).toBeNull;
+  });
+
+  test('INPUT EVENT', async() => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(screen.queryByTestId('value-elem')).toContainHTML('');
+    // Искусственное событие
+    // fireEvent.input(input, {
+    //   target: {value: '123123'}
+    // });
+    // Воспроизводит действия пользователя, обрабатываются нажатия клавиш и т.д.
+    userEvent.type(input, '123123');
+    expect(screen.queryByTestId('value-elem')).toContainHTML('123123');
+  });
+})
+
